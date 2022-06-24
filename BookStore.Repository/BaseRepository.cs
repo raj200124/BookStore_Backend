c2c8f3ae2d2b7d0f4a7169ae7d41988664b402e2
raj200124 <71431499+raj200124@users.noreply.github.com>
2022-06-24 09:35:09 +0530

    Add files via upload

---
+++ BookStore.Repository/BaseRepository.cs
@@ -0,0 +1,9 @@
+﻿using BookStore.Models.ViewModels;
+
+namespace BookStore.Repository
+{
+    public class BaseRepository
+    {
+        protected readonly mybookstoreContext _context = new mybookstoreContext();
+    }
+}
